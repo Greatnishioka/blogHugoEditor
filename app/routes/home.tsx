466cbedcedc56b52e97d55fc57990bb1f438c776
Greatenishioka <20e1118@andrew.ac.jp>
2025-04-30 02:02:02 +0900

    first commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,4 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +8,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+  <>
+      <button className="w-[500px] h-[500px] i-mdi-heart inline-block text-red hover:text-[#000] transition-colors duration-100 ease-linear">aaa</button>
+  </>
+  );
 }
---
+++ uno.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, presetIcons } from 'unocss';
+import presetWind3 from '@unocss/preset-wind3'
+
+const config = {
+  content: {
+    filesystem: [
+      'app/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}',
+    ],
+  },
+  presets: [
+    presetWind3(),
+    presetIcons(),
+  ],
+};
+export default defineConfig(config);
